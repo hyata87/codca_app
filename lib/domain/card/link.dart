2460814315f85d49e82ba4957ff9c37458a8ddf3
hyata87 <disdesdeath@gmail.com>
2020-04-10 22:02:24 +0900

    fix import

--- lib/domain/card/link.dart
+++ lib/domain/card/link.dart
@@ -1,7 +1,5 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-import 'package:codca/domain/maybe.dart';
 import 'package:codca/domain/serializers.dart';
 
 part 'link.g.dart';
