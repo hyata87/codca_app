30ed3243bb2b0d9037d986b014178aa21d72c80f
hyata87 <disdesdeath@gmail.com>
2020-04-23 20:15:26 +0900

    add encrypted google-services.json

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+  --output $HOME/android/app/google-services.json $HOME/.github/secrets/google-services.json.gpg
