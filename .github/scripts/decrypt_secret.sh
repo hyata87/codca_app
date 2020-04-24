a387708dcb0fa7da9c49812ea14364bf1c158c98
hyata87 <disdesdeath@gmail.com>
2020-04-24 11:29:26 +0900

    fix script

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-gpg --version
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output "$HOME/android/app/google-services.json" "$HOME/.github/secrets/google-services.json.gpg"
